1e700e8bd8eeac133b656949e4da9817985dce4f
Eric Peterson <elpeterson16@gmail.com>
2018-09-08 20:31:27 -0400

    updated navigation and routes with future pages, started about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,74 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 // Angular Material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { AboutComponent } from './components/about/about.component';
+import { CodeComponent } from './components/code/code.component';
+import { ComputersComponent } from './components/computers/computers.component';
+import { KeyboardsComponent } from './components/keyboards/keyboards.component';
+import { ResumeComponent } from './components/resume/resume.component';
+
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'code',
+    component: CodeComponent
+  },
+  {
+    path: 'computers',
+    component: ComputersComponent
+  },
+  {
+    path: 'keyboards',
+    component: KeyboardsComponent
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent
+  },
+  {
+    path: '',
+    redirectTo: '/about',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    AboutComponent,
+    ComputersComponent,
+    KeyboardsComponent,
+    CodeComponent,
+    PageNotFoundComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
